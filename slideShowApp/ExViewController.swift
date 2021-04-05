509c835270b993190c05054d0626ca1f56212ec9
kawamey <chocolattecandy1846@icloud.com>
2021-04-05 20:51:53 +0900

    再修正版

--- slideShowApp/ExViewController.swift
+++ slideShowApp/ExViewController.swift
@@ -11,22 +11,21 @@ class ExViewController: UIViewController {
     @IBOutlet weak var exImageView: UIImageView!
     //受け取るためのプロパティの宣言
     var image : UIImage!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         exImageView.image = image
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- slideShowApp/ViewController.swift
+++ slideShowApp/ViewController.swift
@@ -20,13 +20,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     @IBAction func nextImage(_ sender: Any) {
-         imageIndex += 1 //ボタンを押したらimageIndexに１追加していく
+        imageIndex += 1 //ボタンを押したらimageIndexに１追加していく
         //imageIndexが2より大きくなったらimageIndexは0にする
-         if imageIndex > 2 {
-             imageIndex = 0
-         }
+        if imageIndex > 2 {
+            imageIndex = 0
+        }
         //インスタンス名.プロパティ名 = 値   *値には”配列名[添字]”で配列の要素を取り出す
-         imageView.image = images[imageIndex]
+        imageView.image = images[imageIndex]
     }
     
     @IBOutlet weak var backButton: UIButton!
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
         //imageIndexが0より小さくなったらimageIndexは2にする
         if imageIndex < 0 {
             imageIndex = 2
-    }
+        }
         //インスタンス名.プロパティ名 = 値   *値には”配列名[添字]”で配列の要素を取り出す
         imageView.image = images[imageIndex]
     }
@@ -51,25 +51,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBAction func slideShowButton(_ sender: Any) {
         //再生中か停止しているかを判定
-       if (timer == nil) {
-        //再生時の処理を実装
-        //タイマーをセットする
-        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImages), userInfo: nil, repeats: true)
-        //ボタンの名前を停止に変える
-        startButton.setTitle("停止", for: .normal)
-        //他のボタンを無効化
-        nextButton.isEnabled = false
-        backButton.isEnabled = false
-       }else{
-        //再生中か停止しているかを判定
-        //停止時の処理を実装
-        self.timer!.invalidate()
-        //ボタンの名前を再生に変える
-        startButton.setTitle("再生", for: .normal)
-        //他のボタンを有効にする
-        nextButton.isEnabled = true
-        backButton.isEnabled = true
-       }
+        if (timer == nil)  {
+            //再生時の処理を実装
+            //タイマーをセットする
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImages), userInfo: nil, repeats: true)
+            //ボタンの名前を停止に変える
+            startButton.setTitle("停止", for: .normal)
+            //他のボタンを無効化
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+        }else{
+            //再生中か停止しているかを判定
+            //停止時の処理を実装
+            if self.timer != nil {
+                self.timer!.invalidate()
+                self.timer = nil
+                //ボタンの名前を再生に変える
+                startButton.setTitle("再生", for: .normal)
+                //他のボタンを有効にする
+                nextButton.isEnabled = true
+                backButton.isEnabled = true
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,13 +82,22 @@ class ViewController: UIViewController {
     @IBAction func exImage(_ sender: Any) {
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        segueから遷移先のExViewControllerを取得する
+        //        segueから遷移先のExViewControllerを取得する
         let exViewController:ExViewController = segue.destination as! ExViewController
         exViewController.image = images[imageIndex]
-        
+        //停止時の処理を実装
+        if self.timer != nil {
+            self.timer!.invalidate()
+            self.timer = nil
+            //ボタンの名前を再生に変える
+            startButton.setTitle("再生", for: .normal)
+            //他のボタンを有効にする
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+        }
     }
-    @IBAction func unwind(_ segue: UIStoryboardSegue){
-        imageView.image = images[imageIndex]
-    }
+@IBAction func unwind(_ segue: UIStoryboardSegue){
+    imageView.image = images[imageIndex]
+}
 }
 
